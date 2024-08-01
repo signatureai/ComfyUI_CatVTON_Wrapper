23f407eaf301d7734cce5bf0316c379830643b1a
Marco <marco.joao@signature.ai>
2024-08-01 13:31:23 +0100

    expose inpaint checkpoint and resolution

--- py/cat_vton.py
+++ py/cat_vton.py
@@ -9,15 +9,20 @@ class LS_CatVTON:
 
     @classmethod
     def INPUT_TYPES(cls):
+        checkpoints = ["None"] + folder_paths.get_filename_list("checkpoints")
         return {
             "required": {
+                "inpaint_checkpoint": (checkpoints,),
                 "image": ("IMAGE",),
                 "mask": ("MASK",),
                 "refer_image": ("IMAGE",),
+                "width": ("INT", {"default": 768}),
+                "height": ("INT", {"default": 1024}),
+                "mask_blur": ("INT", {"default": 9}),
                 "mask_grow": ("INT", {"default": 25, "min": -999, "max": 999, "step": 1}),
                 "mixed_precision": (["fp32", "fp16", "bf16"], {"default": "fp16"}),
                 "seed": ("INT", {"default": 0, "min": 0, "max": 0xFFFFFFFFFFFFFFFF}),
-                "steps": ("INT", {"default": 40, "min": 1, "max": 10000}),
+                "steps": ("INT", {"default": 50, "min": 1, "max": 10000}),
                 "cfg": ("FLOAT", {"default": 2.5, "min": 0.0, "max": 14.0, "step": 0.1, "round": 0.01,},),
             }
         }
@@ -27,10 +32,10 @@ class LS_CatVTON:
     FUNCTION = "catvton"
     CATEGORY = '😺dzNodes/CatVTON Wrapper'
 
-    def catvton(self, image, mask, refer_image, mask_grow, mixed_precision, seed, steps, cfg):
+    def catvton(self, inpaint_checkpoint, image, mask, refer_image, width, height, mask_blur, mask_grow, mixed_precision, seed, steps, cfg):
 
         catvton_path = os.path.join(folder_paths.models_dir, "CatVTON")
-        sd15_inpaint_path = os.path.join(catvton_path, "stable-diffusion-inpainting")
+
 
         mixed_precision = {
             "fp32": torch.float32,
@@ -39,7 +44,7 @@ class LS_CatVTON:
         }[mixed_precision]
 
         pipeline = CatVTONPipeline(
-            base_ckpt=sd15_inpaint_path,
+            base_ckpt=inpaint_checkpoint,
             attn_ckpt=catvton_path,
             attn_ckpt_version="mix",
             weight_dtype=mixed_precision,
@@ -61,12 +66,12 @@ class LS_CatVTON:
         mask_image = to_pil_image(mask_image)
 
         generator = torch.Generator(device='cuda').manual_seed(seed)
-        person_image, person_image_bbox = resize_and_padding_image(target_image, (768, 1024))
-        cloth_image, _ = resize_and_padding_image(refer_image, (768, 1024))
-        mask, _ = resize_and_padding_image(mask_image, (768, 1024))
+        person_image, person_image_bbox = resize_and_padding_image(target_image, (width, height))
+        cloth_image, _ = resize_and_padding_image(refer_image, (width, height))
+        mask, _ = resize_and_padding_image(mask_image, (width, height))
         mask_processor = VaeImageProcessor(vae_scale_factor=8, do_normalize=False, do_binarize=True,
                                            do_convert_grayscale=True)
-        mask = mask_processor.blur(mask, blur_factor=9)
+        mask = mask_processor.blur(mask, blur_factor=mask_blur)
 
         # Inference
         result_image = pipeline(
